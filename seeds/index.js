661ed054c722369104d414475936f074ea53d74d
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2022-10-03 20:06:17 +0200

    a file using as a seed to generate random cities for testing

---
+++ seeds/index.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const {descriptors, places} = require('./seedHelpers');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
+
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 300; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '6321e0205dc8672320a1eb64',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, repellendus ea? Hic, obcaecati minima doloribus consequuntur veritatis ducimus, eius exercitationem dignissimos aspernatur cupiditate quasi fugiat nostrum illum velit, quae qui?',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [cities[random1000].longitude, cities[random1000].latitude]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                },
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                }
+            ]
+        })
+        await camp.save();
+    }
+}
+
+seedDB();
